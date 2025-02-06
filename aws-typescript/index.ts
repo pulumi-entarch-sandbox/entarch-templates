4d7bf94b528ef121feeb5ae33f39d4e37d814478
Zach Buchheit <zbuchheit@pulumi.com>
2025-02-06 10:25:30 -0800

    tweak the template to set up stacks

--- aws-typescript/index.ts
+++ aws-typescript/index.ts
@@ -1,9 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import { StackSettings } from "./stackSettings";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.BucketV2("my-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+const stackSettings = new StackSettings("my-stack-settings", {
+    driftManagement: "True",
+});
