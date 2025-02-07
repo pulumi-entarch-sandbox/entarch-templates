afcd538c4cc842f25b8244cf91072a1c8d6c9959
Zach Buchheit <zbuchheit@pulumi.com>
2025-02-06 16:55:27 -0800

    Add the stack team to the template for the stack settings.

--- aws-typescript/index.ts
+++ aws-typescript/index.ts
@@ -1,14 +1,40 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-import { StackSettings } from "./stackSettings";
+import * as pulumiservice from "@pulumi/pulumiservice";
+
+const config = new pulumi.Config();
+
+const pulumiStackTeam = config.get("pulumiStackTeam") || "DevTeam";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.BucketV2("my-bucket");
 
-// Export the name of the bucket
 export const bucketName = bucket.id;
 
-const stackSettings = new StackSettings("my-stack-settings", {
-    driftManagement: "True",
-});
+const hoursToAdd = 8;
+const expirationTime = new Date(Date.now() + hoursToAdd * 60 * 60 * 1000).toISOString().slice(0, -7) + "00Z";
+
+new pulumiservice.TtlSchedule(
+    `${pulumi.getProject}-ttlschedule`,
+    {
+        organization: pulumi.getOrganization(),
+        project: pulumi.getProject(),
+        stack: pulumi.getStack(),
+        timestamp: expirationTime,
+        deleteAfterDestroy: false,
+    },
+    { ignoreChanges: ["timestamp"], retainOnDelete: true },
+);
+
+new pulumiservice.TeamStackPermission(
+    `${pulumi.getProject}-team-stack-assign`,
+    {
+        organization: pulumi.getOrganization(),
+        project: pulumi.getProject(),
+        stack: pulumi.getStack(),
+        team: pulumiStackTeam,
+        permission: pulumiservice.TeamStackPermissionScope.Admin,
+    },
+    { retainOnDelete: true },
+);
